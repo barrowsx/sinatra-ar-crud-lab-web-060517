ed56b637062e8e28e8274caf8c60ceb7e01ef71f
barrowsx <wwboyer14@mail.strakejesuit.org>
2017-06-29 13:46:05 -0400

    𝔠𝔬𝔪𝔪𝔦𝔱𝔱𝔢𝔡 𝔲𝔰𝔦𝔫𝔤 𝔞𝔯𝔠𝔞𝔫𝔢 𝔪𝔞𝔤𝔦𝔠𝔨𝔰 𝔩𝔬𝔰𝔱 𝔱𝔬 𝔱𝔥𝔦𝔰 𝔪𝔬𝔯𝔱𝔞𝔩 𝔠𝔬𝔦𝔩

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,53 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  get '/' do
+    redirect '/posts'
+  end
+
+  get '/posts/new' do
+    erb :new
+  end
+
+  post '/posts' do
+    @post = Post.create(params[:post])
+    redirect "/posts"
+  end
+
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+
+  get '/posts/' do
+    redirect '/posts'
+  end
+
+  get '/posts/:id' do
+    begin
+      @post = Post.find(params[:id])
+      erb :show
+    rescue
+      status 404
+      erb :not_found
+    end
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find(params[:id])
+    erb :edit
+  end
+
+  patch '/posts/:id' do
+    @post = Post.find(params[:id])
+    @post.update(params[:post])
+    # binding.pry
+    erb :show
+  end
+
+  delete '/posts/:id/delete' do
+    @post = Post.find(params[:id])
+    Post.find(params[:id]).destroy
+    erb :delete
   end
 end
